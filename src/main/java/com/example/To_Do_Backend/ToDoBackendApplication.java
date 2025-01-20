c481d0bf0992b69c9014db60470dd4c05f17b0b1
RC <chormalhrushikesh83@gmail.com>
2025-01-20 14:28:36 +0530

    Initial commit

---
+++ src/main/java/com/example/To_Do_Backend/ToDoBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.To_Do_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoBackendApplication.class, args);
+		System.out.println("hello");
+		System.out.println("hello world ");
+	}
+
+}

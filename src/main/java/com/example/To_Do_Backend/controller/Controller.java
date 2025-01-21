40c8ace3d719eef4c12f2245ee81e5d37b703423
RC <chormalhrushikesh83@gmail.com>
2025-01-21 17:36:28 +0530

    packages and class created

--- src/main/java/com/example/To_Do_Backend/controller/Controller.java
+++ src/main/java/com/example/To_Do_Backend/controller/Controller.java
@@ -1,10 +1,12 @@
 package com.example.To_Do_Backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin
 public class Controller {
 
     @GetMapping("/home")    
---
+++ src/main/java/com/example/To_Do_Backend/service/TodoService.java
@@ -0,0 +1,8 @@
+package com.example.To_Do_Backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+}
